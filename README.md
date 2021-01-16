18ca62f7bab61c0f1f4c4d2b174ffff2ce2631e6
Helge Gudmundsen <helgeg@helgegudmundsen.com>
2021-01-16 19:24:39 +0000

    Spelling in README

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ It requires two environment variables to be set:
 
 `IDONTIME_USER` and `IDONTIME_PASSWORD` that should contain the username and password used to log in to the time keeping system.
 
-By default, the script will generate IN and OUT entries for the current day, with entry at 9am, enx exit at 6pm.
+By default, the script will generate IN and OUT entries for the current day, with entry at 9am, and exit at 6pm.
 
-The script can be overriden, by entering a date, a time in, and a time out in the following format:
+The script can be overridden, by entering a date, a time in, and a time out in the following format:
 
 `ruby idontime.rb [dd-mm-yyyy] [hh:mm] [hh:mm]`
 
--- idontime.rb
+++ idontime.rb
@@ -8,17 +8,28 @@ props = YAML.safe_load(File.read(PROPERTIES_FILE))
 user = ENV['IDONTIME_USER']
 password = ENV['IDONTIME_PASSWORD']
 
+def create_entry(driver, type, props)
+  puts "Clocking #{type}"
+  go_to_url(driver, props['mov_url'])
+  wait
+  click(driver, props['add_button'])
+  wait
+  frame(driver, props['add_frame'])
+  find_by_id_clear_and_fill_in(driver, props['date_field'], props['date_to_add'])
+  # Set the entry type in the drop down
+  # submit
+end
+
 driver = create_driver
+
 go_to_url(driver, props['url'])
 find_by_id_and_fill_in(driver, props['user_field'], user)
 find_by_id_and_fill_in(driver, props['password_field'], password)
 click(driver, props['login_button'])
-go_to_url(driver, props['mov_url'])
-wait
-click(driver, props['add_button'])
-wait
-frame(driver, props['add_frame'])
-find_by_id_clear_and_fill_in(driver, props['date_field'], props['date_to_add'])
+# create IN post
+create_entry(driver, 'in', props)
+# create OUT post
+create_entry(driver, 'out', props)
 
 puts driver.title
 
